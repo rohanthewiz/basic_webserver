d60ff16d393e3ca7f5f1e6eb75c08d3f11eb431e
Rohan Allison <rohanthewiz_at_gmail.com>
2016-11-28 18:27:38 -0600

    Add a JSON endpoint - just bc it's so easy

--- src/basic_webserver.cr
+++ src/basic_webserver.cr
@@ -1,9 +1,13 @@
-require "./basic_webserver/*"
 require "kemal"
 
 module BasicWebserver
   get "/" do
 	"I'm up!"
+  end
+
+  get "/api" do |context|
+	context.response.content_type = "application/json"
+	{status: "OK", msg: "I'm running"}.to_json
   end
 end
 
